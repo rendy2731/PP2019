4936024e8bc58bbe0821ae6a355ce4266594bf54
septiintanaml <septiintana@gmail.com>
2019-11-05 21:13:59 +0800

    collection

---
+++ H071191022/Praktikum7/P7_2.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class P7_2 {
+
+    public static void main(String[] args) {
+        Scanner sep = new Scanner (System.in);
+        String input = sep.nextLine();
+        HashMap<String,Character> morse = new HashMap<>();
+        String [] space = input.split("\\s+");
+
+        morse.put(".-", 'A');
+        morse.put("-...", 'B');
+        morse.put("-.-.",'C');
+        morse.put("-..",'D');
+        morse.put(".",'E');
+        morse.put(".._.",'F');
+        morse.put("--.",'G');
+        morse.put("....",'H');
+        morse.put("..",'I');
+        morse.put(".---",'J');
+        morse.put("-.-",'K');
+        morse.put(".-..",'L');
+        morse.put("--",'M');
+        morse.put("-.",'N');
+        morse.put("---",'O');
+        morse.put(".--.",'P');
+        morse.put("--.-",'Q');
+        morse.put(".-.",'R');
+        morse.put("...",'S');
+        morse.put("-",'T');
+        morse.put("..-",'U');
+        morse.put("...-",'V');
+        morse.put(".--",'W');
+        morse.put("-..-", 'X');
+        morse.put("-.--",'Y');
+        morse.put("--..",'Z');
+        
+        for (String result: space){
+            System.out.print(morse.get(result));
+        }  
+    }
+}
